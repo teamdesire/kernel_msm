02614e3c9450e2304c634288a2e773694d630979
Diogo Ferreira <diogo@underdev.org>
2010-06-05 20:48:51 +0100

    Externalize constants and expose i2c to external drivers

--- arch/arm/mach-msm/board-bravo-microp.c
+++ arch/arm/mach-msm/board-bravo-microp.c
@@ -44,74 +44,11 @@
 #include <linux/mutex.h>
 #include <linux/jiffies.h>
 
+#include <mach/board-bravo-microp-common.h>
+
 #include "board-bravo.h"
 
 
-#define MICROP_I2C_NAME "bravo-microp"
-
-#define MICROP_LSENSOR_ADC_CHAN		6
-#define MICROP_REMOTE_KEY_ADC_CHAN	7
-
-#define MICROP_I2C_WCMD_MISC				0x20
-#define MICROP_I2C_WCMD_SPI_EN				0x21
-#define MICROP_I2C_WCMD_AUTO_BL_CTL			0x23
-#define MICROP_I2C_RCMD_SPI_BL_STATUS			0x24
-#define MICROP_I2C_WCMD_BUTTONS_LED_CTRL		0x25
-#define MICROP_I2C_RCMD_VERSION				0x30
-#define MICROP_I2C_WCMD_ADC_TABLE			0x42
-#define MICROP_I2C_WCMD_LED_MODE			0x53
-#define MICROP_I2C_RCMD_GREEN_LED_REMAIN_TIME		0x54
-#define MICROP_I2C_RCMD_AMBER_RED_LED_REMAIN_TIME	0x55
-#define MICROP_I2C_RCMD_BLUE_LED_REMAIN_TIME		0x57
-#define MICROP_I2C_WCMD_JOGBALL_LED_MODE		0x5A
-#define MICROP_I2C_RCMD_JOGBALL_LED_REMAIN_TIME		0x5B
-#define MICROP_I2C_WCMD_JOGBALL_LED_PWM_SET		0x5C
-#define MICROP_I2C_WCMD_JOGBALL_LED_PERIOD_SET		0x5D
-#define MICROP_I2C_WCMD_READ_ADC_VALUE_REQ		0x60
-#define MICROP_I2C_RCMD_ADC_VALUE			0x62
-#define MICROP_I2C_WCMD_REMOTEKEY_TABLE			0x63
-#define MICROP_I2C_WCMD_LCM_REGISTER			0x70
-#define MICROP_I2C_WCMD_GSENSOR_REG			0x73
-#define MICROP_I2C_WCMD_GSENSOR_REG_DATA_REQ		0x74
-#define MICROP_I2C_RCMD_GSENSOR_REG_DATA		0x75
-#define MICROP_I2C_WCMD_GSENSOR_DATA_REQ		0x76
-#define MICROP_I2C_RCMD_GSENSOR_X_DATA			0x77
-#define MICROP_I2C_RCMD_GSENSOR_Y_DATA			0x78
-#define MICROP_I2C_RCMD_GSENSOR_Z_DATA			0x79
-#define MICROP_I2C_RCMD_GSENSOR_DATA			0x7A
-#define MICROP_I2C_WCMD_OJ_REG				0x7B
-#define MICROP_I2C_WCMD_OJ_REG_DATA_REQ			0x7C
-#define MICROP_I2C_RCMD_OJ_REG_DATA			0x7D
-#define MICROP_I2C_WCMD_OJ_POS_DATA_REQ			0x7E
-#define MICROP_I2C_RCMD_OJ_POS_DATA			0x7F
-#define MICROP_I2C_WCMD_GPI_INT_CTL_EN			0x80
-#define MICROP_I2C_WCMD_GPI_INT_CTL_DIS			0x81
-#define MICROP_I2C_RCMD_GPI_INT_STATUS			0x82
-#define MICROP_I2C_RCMD_GPI_STATUS			0x83
-#define MICROP_I2C_WCMD_GPI_INT_STATUS_CLR		0x84
-#define MICROP_I2C_RCMD_GPI_INT_SETTING			0x85
-#define MICROP_I2C_RCMD_REMOTE_KEYCODE			0x87
-#define MICROP_I2C_WCMD_REMOTE_KEY_DEBN_TIME		0x88
-#define MICROP_I2C_WCMD_REMOTE_PLUG_DEBN_TIME		0x89
-#define MICROP_I2C_WCMD_SIMCARD_DEBN_TIME		0x8A
-#define MICROP_I2C_WCMD_GPO_LED_STATUS_EN		0x90
-#define MICROP_I2C_WCMD_GPO_LED_STATUS_DIS		0x91
-
-/* Desire */
-#define IRQ_OJ 		(1<<12)  // verified in 2.6.29
-#define IRQ_GSENSOR     (1<<10)  // not verified
-#define IRQ_LSENSOR     (1<<9)   // non verified
-#define IRQ_REMOTEKEY   (1<<7)	 // verified in 2.6.29
-#define IRQ_HEADSETIN	(1<<2)	 // verified in 2.6.29
-#define IRQ_SDCARD      (1<<0)   // verified in 2.6.29
-
-/* Nexus
-#define IRQ_GSENSOR	(1<<10)
-#define IRQ_LSENSOR  	(1<<9)
-#define IRQ_REMOTEKEY	(1<<7)
-#define IRQ_HEADSETIN	(1<<2)
-#define IRQ_SDCARD	(1<<0)
-*/
 
 #define READ_GPI_STATE_HPIN	(1<<2)
 #define READ_GPI_STATE_SDCARD	(1<<0)
@@ -377,6 +314,42 @@ static int i2c_write_block(struct i2c_client *client, uint8_t addr,
 			I2C_WRITE_RETRY_TIMES);
 	return -EIO;
 }
+
+int microp_i2c_read(uint8_t addr, uint8_t *data, int length)
+{
+	struct i2c_client *client = private_microp_client;
+
+	if (!client)	{
+		printk(KERN_ERR "%s: dataset: client is empty\n", __func__);
+		return -EIO;
+	}
+
+	if (i2c_read_block(client, addr, data, length) < 0)	{
+		dev_err(&client->dev, "%s: write microp i2c fail\n", __func__);
+		return -EIO;
+	}
+
+	return 0;
+}
+EXPORT_SYMBOL(microp_i2c_read);
+
+int microp_i2c_write(uint8_t addr, uint8_t *data, int length)
+{
+	struct i2c_client *client = private_microp_client;
+
+	if (!client)	{
+		printk(KERN_ERR "%s: dataset: client is empty\n", __func__);
+		return -EIO;
+	}
+
+	if (i2c_write_block(client, addr, data, length) < 0)	{
+		dev_err(&client->dev, "%s: write microp i2c fail\n", __func__);
+		return -EIO;
+	}
+
+	return 0;
+}
+EXPORT_SYMBOL(microp_i2c_write);
 
 static int microp_read_adc(uint8_t channel, uint16_t *value)
 {
