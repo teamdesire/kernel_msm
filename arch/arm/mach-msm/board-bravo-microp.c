2fbde003cef2a2b52bf436d3b67fdd686fbdf44b
Diogo Ferreira <diogo@underdev.org>
2010-06-22 14:10:13 +0100

    [OJ+GSensor] Fix intermitent shutdown of optical joystick

--- arch/arm/mach-msm/board-bravo-microp.c
+++ arch/arm/mach-msm/board-bravo-microp.c
@@ -1,4 +1,5 @@
 /* board-bravo-microp.c
+ *
  * Copyright (C) 2009 Google.
  * Copyright (C) 2009 HTC Corporation.
  *
@@ -220,6 +221,7 @@ struct microp_i2c_client_data {
 	uint32_t als_kadc;
 	uint32_t als_gadc;
 	uint8_t als_calibrating;
+	uint32_t spi_devices_enabled;
 };
 
 static char *hex2string(uint8_t *data, int len)
@@ -1357,13 +1359,28 @@ static int microp_spi_enable(uint8_t on)
 	return ret;
 }
 
+/* Lookup active SPI devices and only turn it off when no device
+ * is using it
+ * */
 int microp_spi_vote_enable(int spi_device, uint8_t enable) {
 	//XXX need to check that all that crap in the HTC kernel is needed
 	struct i2c_client *client = private_microp_client;
+	struct microp_i2c_client_data *cdata = i2c_get_clientdata(client);
 	int ret;
 
 	if (spi_device == SPI_OJ)
 		microp_oj_intr_enable(client, enable);
+
+	/* Add/remove it from the poll */
+	if (enable)
+		cdata->spi_devices_enabled |= spi_device;
+	else
+		cdata->spi_devices_enabled &= ~spi_device;
+
+	if (cdata->spi_devices_enabled)
+		enable = 1;
+	else enable = 0;
+
 
 	ret = microp_spi_enable(enable);
 	return ret;
@@ -1479,7 +1496,7 @@ static int gsensor_init_hw(void)
 
 	pr_debug("%s\n", __func__);
 
-	microp_spi_enable(1);
+	microp_spi_vote_enable(SPI_GSENSOR, 1);
 
 	ret = gsensor_read_reg(RANGE_BWIDTH_REG, &reg);
 	if (ret < 0 )
@@ -1505,7 +1522,7 @@ static int bma150_set_mode(char mode)
 
 	pr_debug("%s mode = %d\n", __func__, mode);
 	if (mode == BMA_MODE_NORMAL)
-		microp_spi_enable(1);
+		microp_spi_vote_enable(SPI_GSENSOR, 1);
 
 
 	ret = gsensor_read_reg(SMB150_CTRL_REG, &reg);
@@ -1515,7 +1532,7 @@ static int bma150_set_mode(char mode)
 	ret = gsensor_write_reg(SMB150_CTRL_REG, reg);
 
 	if (mode == BMA_MODE_SLEEP)
-		microp_spi_enable(0);
+		microp_spi_vote_enable(SPI_GSENSOR, 0);
 
 	return ret;
 }
@@ -2005,6 +2022,7 @@ static int microp_i2c_probe(struct i2c_client *client,
 	cdata->microp_is_suspend = 0;
 	cdata->auto_backlight_enabled = 0;
 	cdata->light_sensor_enabled = 0;
+	cdata->spi_devices_enabled = 0;
 
 	wake_lock_init(&microp_i2c_wakelock, WAKE_LOCK_SUSPEND,
 			 "microp_i2c_present");
